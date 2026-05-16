26ff73ab73cd84ab1586cbb9dabbba4ef630d055
hokosary <hokosary3472@gmail.com>
2026-05-16 06:17:34 +0300

    load app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MapScreen from './screens/MapScreen';
+
+export default function App() {
+  return (
+    <div style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
+      <MapScreen />
+    </div>
+  );
+}
